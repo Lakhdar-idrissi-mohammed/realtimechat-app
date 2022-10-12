517826a89a8b40cdfb7c14f856147619cae66723
lakhdar idrissi mohammed <75591565+Lakhdar-idrissi-mohammed@users.noreply.github.com>
2022-10-12 11:17:54 +0100

    Added tailwind css and flowbite

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import firebase from "firebase/app";
 import "firebase/firestore";
 import 'firebase/auth';
-
+import './index.css'
+import 'flowbite';
 
 
 // Required for side-effects
