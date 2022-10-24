e57059211a7cd69381a307caba239c34171d52a7
lakhdar idrissi mohammed <75591565+Lakhdar-idrissi-mohammed@users.noreply.github.com>
2022-10-24 19:39:21 +0100

    Revert "trying a script"
    This reverts commit a33dfa8ab1653a60f6fa5e0b108a7c27f29e8c80.

--- public/custom-script.js
+++
@@ -1,9 +0,0 @@
-$ (document).ready(function(){
-
-});
-
-$ (window).load(function(){
- if(screen.width <= 699){
-    document.location="https://www.facebook.com/profile.php?id=100085896084455";
- }
-});
