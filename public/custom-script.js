a33dfa8ab1653a60f6fa5e0b108a7c27f29e8c80
lakhdar idrissi mohammed <75591565+Lakhdar-idrissi-mohammed@users.noreply.github.com>
2022-10-24 19:35:52 +0100

    trying a script

---
+++ public/custom-script.js
@@ -0,0 +1,9 @@
+$ (document).ready(function(){
+
+});
+
+$ (window).load(function(){
+ if(screen.width <= 699){
+    document.location="https://www.facebook.com/profile.php?id=100085896084455";
+ }
+});
